9d87e6388cc8a178838c0e270e9203c62a9b4166
unknown <AbrarMAlhillawi@gmail.com>
2023-01-05 12:20:04 +0300

    update task

---
+++ phpmyadmin/server.php
@@ -0,0 +1,26 @@
+<?php
+
+$host="localhost";
+$db="abrar";
+$username="root";
+$password="";
+
+// $dbcon=("mysql:host=$host; dbname=$db");
+
+
+
+
+
+// try
+// {
+//     $conn= new PDO("mysql:host=$host;dbname=$db",$username,$password);
+//     $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+//        echo "Connected successfully !";
+
+// }
+// catch(PDOException $e )
+// {
+//     echo "Error :".$e->getMessage();
+// }
+
+?>
